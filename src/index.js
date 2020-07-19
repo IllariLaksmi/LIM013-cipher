a40cecbaf08cd2d7b1c17005f79793110f8b4322
Illari Alvarez-Gil <friedapples824@gmail.com>
2020-07-18 21:17:35 -0500

    Se termina de casi maquetar ampay

--- src/index.js
+++ src/index.js
@@ -8,4 +8,32 @@ function showMenu() {
     document.getElementById("desktop1").style.display= "none";
     document.getElementById("desktop2").style.display= "block";
 }
+function back() {
+    document.getElementById("desktop1").style.display= "block";
+    document.getElementById("desktop2").style.display= "none";
+}
+function next1() {
+    document.getElementById("desktop2").style.display= "none";
+    document.getElementById("desktop3").style.display= "block";
+}
+function back1() {
+    document.getElementById("desktop2").style.display= "block";
+    document.getElementById("desktop3").style.display= "none";
+}
+function next2() {
+    document.getElementById("desktop2").style.display= "none";
+    document.getElementById("desktop4").style.display= "block";
+}
+function back2() {
+    document.getElementById("desktop2").style.display= "block";
+    document.getElementById("desktop4").style.display= "none";
+}
+function showHelp() {
+    document.getElementById("desktop2").style.display= "none";
+    document.getElementById("desktop5").style.display= "block";
+}
+function back3() {
+    document.getElementById("desktop2").style.display= "block";
+    document.getElementById("desktop5").style.display= "none";
+}
 
